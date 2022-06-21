4d8201ce8f4d5f0f1b6e41d0584565417c49895c
Cristian <cris4599al@gmail.com>
2022-06-21 19:42:31 +0300

    Added profiles for payments

--- Payment/src/main/java/com/example/examen/service/client/CurrencyServiceProxy.java
+++ Payment/src/main/java/com/example/examen/service/client/CurrencyServiceProxy.java
@@ -4,7 +4,8 @@ import com.example.examen.model.Currency;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "currency")
+//@FeignClient(value = "currency-ron")
+@FeignClient(value = "${feign.value}")
 public interface CurrencyServiceProxy
 {
     @GetMapping(value="/currency", consumes = "application/json")
